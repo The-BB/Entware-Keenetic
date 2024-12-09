3e8ae80815ad32ae46ffdaaddd15e5d854ec9f3f
The-BB <tun.chen.bo@gmail.com>
2024-12-09 12:12:53 +0300

    dl_check.sh: add warn

--- dl_check.sh
+++ dl_check.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
+[ -x "./staging_dir/host/bin/zstd" ] || \
+{ printf '\033[31m%s\033[m' "WARNING: some tools are missing, please run \"make tools\" before re-running this script." && \
+  exit 1 ; }
+
 if [ -e .config ]; then
-    read -p 'Please note: .config will be rewriten during downloading! Press Ctrl+C to abort or Enter to continue' i
+    read -p 'Please note: .config will be rewriten during downloading! Press [Ctrl]+[C] to abort or [Enter] to continue... ' key
 fi
 
-for entware_target in $(ls -1 configs); do
+for entware_target in ./configs/*.config; do
+    [ -e "$entware_target" ] || break
     echo "Getting $entware_target sources..."
-    cp configs/$entware_target .config
-    make -j4 download V=s
-    if [ $? -ne 0 ]; then
+    cp "$entware_target" .config
+    if ! make -j"$(nproc)" download V=s; then
         echo "$openwrt_target download failed for $entware_target:("
         exit 1
     fi
